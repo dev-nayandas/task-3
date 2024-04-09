351e7cbbe213e1e52a1c9517fd54ec61fb9f3211
dev-nayandas <rich.nayan@gmail.com>
2024-04-09 16:41:13 +0600

    done

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function GMfun() {
+    const result = confirm("I am an alert box!");
+     if (result) {
+         document.querySelector('#gm-heading-wrap').style.backgroundColor = 'orange';
+       }
+   }
+   
